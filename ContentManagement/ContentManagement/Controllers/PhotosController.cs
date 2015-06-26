ed2f64743e33dd32bc4b1576d51b9253a2b0206a
Mizanur Rahman Milon <milon06cse@gmail.com>
2015-06-26 15:36:26 +0600

    sliding done

---
+++ ContentManagement/ContentManagement/Controllers/PhotosController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ContentManagement.Models;
+
+namespace ContentManagement.Controllers
+{
+    public class PhotosController : Controller
+    {
+        //
+        // GET: /Photos/
+
+        public ActionResult Index()
+        {
+            PhotosModel model = new PhotosModel();
+            return View(model.PhotosByParentId().ToList());
+        }
+
+    }
+}
---
+++ ContentManagement/ContentManagement/Models/PhotosModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SCM.CLL;
+using SCM.DAL;
+
+namespace ContentManagement.Models
+{
+    public class PhotosModel
+    {
+        public Guid PhotoId { get; set; }
+        public Guid ParentId { get; set; }
+        public string Title { get; set; }
+        public string SmallDescription { get; set; }
+        public string AlternativeText { get; set; }
+        public string Photourl { get; set; }
+        public List<PhotosModel> PhotosByParentId()
+        {List<PhotosModel> Modelitems=new List<PhotosModel>();
+            List<Photos> items = new PhotosRepository().ShowPhotosByParentId(new Guid("59ef2601-e4e4-423e-8bb7-d5b1d589f028"));
+            foreach (Photos item in items)
+            { PhotosModel modelitem =ClassToModel( item); Modelitems.Add(modelitem); }
+            return Modelitems;
+        }
+      internal PhotosModel ClassToModel(Photos item)
+      {
+          PhotosModel  mitem=new PhotosModel();
+          mitem.PhotoId = item.PhotoId;
+          mitem.ParentId = item.ParentId;
+          mitem.Title = item.Title;
+          mitem.SmallDescription = item.SmallDescription;
+          mitem.AlternativeText = item.AlternativeText;
+          mitem.Photourl ="images/"+item.PhotoId+".jpg";
+          return mitem;
+      }
+      // string url=@"<img src="\"images/2.jpg" alt="""second image""" width="""460""" height="""345"">";
+               
+    }
+}
